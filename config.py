8362c03806f2f746c953b9765f4679f50db62c76
hridhya <menonhridhya@gmail.com>
2015-09-11 14:36:48 -0700

    Changed db to postgreSQL

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ OPENID_PROVIDERS = [
     
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'mysql://surveyadmin:survey@localhost/surveyapp'
+SQLALCHEMY_DATABASE_URI =os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
